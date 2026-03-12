00ceaf33661c9b7c6502217f0664ece0d8a77919
Mithun N <mithhhunn@gmail.com>
2026-03-12 11:08:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
+// serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
+// homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(port, () => {
-  console.log("Server running on port " + port);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
